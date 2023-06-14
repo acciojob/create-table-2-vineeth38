9734b9686c908f6af174bbe105bb0b6cd4b54e45
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-06-14 22:28:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function createTable() {
     //Write your code here
-  
+  var rows = prompt("Input number of rows");
+        var columns = prompt("Input number of columns");
+
+        if (rows && columns) {
+          var table = document.getElementById("myTable");
+
+          // Clear any existing table content
+          table.innerHTML = "";
+
+          for (var i = 0; i < rows; i++) {
+            var row = table.insertRow(i);
+
+            for (var j = 0; j < columns; j++) {
+              var cell = row.insertCell(j);
+              cell.textContent = "Row-" + i + " Column-" + j;
+            }
+          }
+        }
 }
